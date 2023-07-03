d6bedce944a353595325c40ab3dfa6e0725a19d5
Sequoia Kanies <kaniessequoia@gmail.com>
2023-07-03 17:37:11 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+//import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+//import About from './components/About'
+import { useState, useEffect} from 'react'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+  
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  },[])
+
+// Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+
+// Fetch Tasks
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+  return data
+}
+
+// Add Task
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+  const data = await res.json()
+  setTasks([...tasks, data])
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = { id, ...task }
+  // setTasks([...tasks, newTask])
+}
+
+
+// Delete Task
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE',
+  })
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = {...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => 
+    task.id === id ? {...task, reminder:
+    data.reminder} : task
+    )
+  )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header 
+        onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+          {showAddTask && <AddTask onAdd={addTask} />}
+          {tasks.length > 0 ? (
+          <Tasks 
+            tasks={tasks} 
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />) : ( 'No tasks to show')}
+      {/* <Footer /> */}
     </div>
-  );
+  )
 }
 
 export default App;
+
+// Using Routers
+
+// This functionality is broken or outdated. To see the tutorial for this
+//project go to https://www.youtube.com/watch?v=w7ejDZ8SWv8
+// YouTube video 'ReactJS Crash Course' by Traversy Media
+
+// <Router>
+//       <div className='container'>
+//         <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+//         <Route path='/' exact render={(props) => (
+//           <>
+//             {showAddTask && <AddTask onAdd={addTask} />}
+//             {tasks.length > 0 ? (<Tasks 
+//             tasks={tasks} 
+//             onDelete={deleteTask}
+//             onToggle={toggleReminder}
+//             />) : ( 'No tasks to show')}
+//           </>
+//         )}
+//         />
+//         <Route path='/about' component={About}/>
+//         <Footer />
+//       </div>
+//     </Router>
+
+
+//To run this project on a local host, open a local 
+//(in this directory using code .) 
+//terminal and use npm run start
